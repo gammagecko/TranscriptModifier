7a2dc436f1f7bed719946411c1b1f4e28481f421
gammagecko <smcknel@gmail.com>
2025-03-08 12:37:20 -0700

    Updated Main/sample output

--- Main.py
+++ Main.py
@@ -4,14 +4,19 @@ with open('SampleTranscript.txt', 'r') as f:
     for line in f:
         line = line.strip()
         if line != "" and line[-1].isdigit() and line[-3] == ":":
-            if len(line) == 4 or len(line) == 5:
-                minutes = int(line[-4:-6:-1])
-
-            if minutes == count:
+            if len(line) == 4:
+                minutes = int(line[-4:-3])
+            elif len(line) == 5:
+                minutes = int(line[-5:-3])
+            elif len(line) > 5:
+                minutes = int(line[-5:-3])
+            if minutes == count or count == 60:
                 count += 1
-                transcript += str(line) + "\n"
                 if count == 61:
                     count = 0
+                if count % 2 == 1:
+                    transcript += str(line) + "\n"
         else:
             transcript += line + "\n"
-print(transcript)
+with open('Transcript.txt', 'w') as file:
+    file.write(transcript)
